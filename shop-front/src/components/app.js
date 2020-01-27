11817b41c32862ca5081c7c108c97302434370eb
bcrave <brennycode@gmail.com>
2020-01-27 10:56:56 -0700

    updated react environment

--- shop-front/src/components/app.js
+++ shop-front/src/components/app.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + Redux</h2>
-
-        <div style={{ color: "red" }}>
-          Warning: this is the experimental template, pardon the bugs
+        <h1>Donut Shop</h1>
+        <div className="navbar">
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </Router>
         </div>
       </div>
     );
