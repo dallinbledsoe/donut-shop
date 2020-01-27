2f2d3f07e7bb655ce525752634aaa136cdd91a79
bcrave <brennycode@gmail.com>
2020-01-27 11:24:09 -0700

    added navigation bar

--- shop-front/src/components/app.js
+++ shop-front/src/components/app.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Donut Shop</h1>
-        <div className="navbar">
+        <div className="page-wrapper">
           <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-            </Switch>
+            <div className="navbar">
+              <h1>Donut Shop</h1>
+              <div className="nav-links-wrapper">
+                <NavLink to="/" activeClassName="nav-link-active">
+                  Home
+                </NavLink>
+                <NavLink to="/about" activeClassName="nav-link-active">
+                  About
+                </NavLink>
+              </div>
+              <div className="login">user</div>
+            </div>
+
+            <div className="page-content">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+              </Switch>
+            </div>
           </Router>
         </div>
       </div>
